e4232d9295669e358d9dcc6056173db7f31d5764
BaltazarMolteni <baltazarmolteni04@gmail.com>
2025-07-07 16:08:39 -0300

    creacion de proyecto

---
+++ app/_layout.tsx
@@ -0,0 +1,18 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '../src/hooks/useColorScheme';
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme()
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+         <Stack screenOptions={{headerShown: false}}/>
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
